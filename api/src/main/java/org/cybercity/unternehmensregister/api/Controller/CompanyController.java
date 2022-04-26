f8858051d19531f7bad0b17f29b01e31dedb9199
Clemens <dubcmsky@protonmail.com>
2022-04-26 03:06:11 +0200

    added set endpoints for the company api

--- api/src/main/java/org/cybercity/unternehmensregister/api/Controller/CompanyController.java
+++ api/src/main/java/org/cybercity/unternehmensregister/api/Controller/CompanyController.java
@@ -4,9 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.cybercity.unternehmensregister.api.Model.Company;
 import org.cybercity.unternehmensregister.api.Service.CompanyService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,6 +21,41 @@ public class CompanyController {
     @GetMapping(path = "getAll", produces = "application/json")
     public List<Company> getAll() {
         return companyService.getAll();
+    }
+
+    @GetMapping(path = "getByID", produces = "application/json")
+    public Company getByID(@RequestParam int id) {
+        return companyService.getByID(id);
+    }
+
+    @GetMapping(path = "getByName", produces = "application/json")
+    public Company getByName(@RequestParam String input) {
+        return companyService.getByName(input);
+    }
+
+    @GetMapping(path = "getByAbbreviation", produces = "application/json")
+    public Company getByAbbreviation(@RequestParam String input) {
+        return companyService.getByAbbreviation(input);
+    }
+
+    @GetMapping(path = "getByCeoId", produces = "application/json")
+    public List<Company> getByCeoId(@RequestParam int id) {
+        return companyService.getByCeoId(id);
+    }
+
+    @PostMapping(path = "setName", produces = "application/json")
+    public void setName(@RequestParam long id, @RequestParam String newName) {
+        companyService.setName(id, newName);
+    }
+
+    @PostMapping(path = "setAbbreviation", produces = "application/json")
+    public void setAbbreviation(@RequestParam long id, @RequestParam String newAbbreviation) {
+        companyService.setAbbreviation(id, newAbbreviation);
+    }
+
+    @PostMapping(path = "setCeoId", produces = "application/json")
+    public void setCeoId(@RequestParam long id, @RequestParam long newCeoId) {
+        companyService.setCeoId(id, newCeoId);
     }
 
 }
--- api/src/main/java/org/cybercity/unternehmensregister/api/Repositories/CompanyRepository.java
+++ api/src/main/java/org/cybercity/unternehmensregister/api/Repositories/CompanyRepository.java
@@ -2,8 +2,23 @@ package org.cybercity.unternehmensregister.api.Repositories;
 
 import org.cybercity.unternehmensregister.api.Model.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CompanyRepository extends JpaRepository<Company, Long> {
+
+    @Query(value = "SELECT c FROM Company c WHERE c.name =: name")
+    Optional<Company> findByName(@Param("name") String name);
+
+    @Query(value = "SELECT c FROM Company c WHERE c.abbrevation =: abbrevation")
+    Optional<Company> findByAbbreviation(@Param("abbrevation") String abbrevation);
+
+    @Query(value = "SELECT c FROM Company c WHERE c.ceo_id =: Ceo_id")
+    List<Company> findByCeoId(@Param("id") long Ceo_id);
+
 }
