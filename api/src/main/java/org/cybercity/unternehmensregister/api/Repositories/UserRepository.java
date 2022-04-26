502f4093b2c261e2bc7074b813c9da2cbfcda4c8
Clemens <dubcmsky@protonmail.com>
2022-04-26 02:08:38 +0200

    added openapi-swagger support

--- api/src/main/java/org/cybercity/unternehmensregister/api/Repositories/UserRepository.java
+++ api/src/main/java/org/cybercity/unternehmensregister/api/Repositories/UserRepository.java
@@ -4,6 +4,9 @@ import org.cybercity.unternehmensregister.api.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * @author Dubsky
+ */
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
--- api/src/main/java/org/cybercity/unternehmensregister/api/Service/UserService.java
+++ api/src/main/java/org/cybercity/unternehmensregister/api/Service/UserService.java
@@ -9,6 +9,9 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * @author Dubsky
+ */
 @Service
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class UserService {
