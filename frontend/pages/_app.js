3251601fce537f126aa6bf6c8c17de3007e4590e
Clemens <dubcmsky@protonmail.com>
2022-04-30 23:04:04 +0200

    update next.js to newer version

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,21 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/globals.css'
+import '../styles/Animated-Type-Heading.css'
+import Head from "next/head";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import {useEffect} from "react";
+
+function MyApp({Component, pageProps}) {
+    useEffect(() => {
+        import("bootstrap/dist/js/bootstrap");
+    }, []);
+
+    <Head>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no"/>
+    </Head>
+
+    return <Component {...pageProps} />
 }
 
 export default MyApp
