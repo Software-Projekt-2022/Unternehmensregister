3bd28923999ee1c8aca496b6e9a9ffb6dcee4cba
Clemens <dubcmsky@protonmail.com>
2022-05-07 23:48:34 +0200

    added JobController and new gradle config

--- api/src/main/java/org/cybercity/unternehmensregister/api/Repositories/ApplicationRepository.java
+++ api/src/main/java/org/cybercity/unternehmensregister/api/Repositories/ApplicationRepository.java
@@ -3,5 +3,5 @@ package org.cybercity.unternehmensregister.api.Repositories;
 import org.cybercity.unternehmensregister.api.Model.Application;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ApplicationRepository extends JpaRepository<Application, Integer> {
+public interface ApplicationRepository extends JpaRepository<Application, Long> {
 }
