f14e509db9eb63550a7d1264e0adfa04b734f146
Clemens <admin@dubskysarchiv.de>
2022-05-13 00:55:49 +0200

    started to transition to request bodies

--- RestApi/src/main/java/org/cybercity/unternehmensregister/restapi/Controller/UserController.java
+++ RestApi/src/main/java/org/cybercity/unternehmensregister/restapi/Controller/UserController.java
@@ -19,42 +19,47 @@ public class UserController {
 
     private final UserService userService;
 
-    @GetMapping(path = "/getUsers", produces = "application/json")
+    @PostMapping(path = "newUser", consumes = "application/json", produces = "application/json")
+    public User newUser(@RequestBody User user) {
+        return userService.newUser(user);
+    }
+
+    @GetMapping(path = "getUsers", produces = "application/json")
     public List<User> getUsers() {
         return userService.getUsers();
     }
 
-    @GetMapping(path = "/getUserByID", produces = "application/json")
-    public User getUser(@RequestParam long id) {
+    @GetMapping(path = "getUserByID/{id}", produces = "application/json")
+    public User getUser(@PathVariable long id) {
         return userService.getUser(id);
     }
 
-    @PostMapping(path = "/setForename", produces = "application/json")
+    @PostMapping(path = "setForename", produces = "application/json")
     public void setForename(@RequestParam long id, @RequestParam String newName) {
         userService.setForename(id, newName);
     }
 
-    @PostMapping(path = "/setSurname", produces = "application/json")
+    @PostMapping(path = "setSurname", produces = "application/json")
     public void setSurname(@RequestParam long id, @RequestParam String newName) {
         userService.setSurname(id, newName);
     }
 
-    @PostMapping(path = "/setAge", produces = "application/json")
+    @PostMapping(path = "setAge", produces = "application/json")
     public void setAge(@RequestParam long id, @RequestParam int newAge) {
         userService.setAge(id, newAge);
     }
 
-    @PostMapping(path = "/setEmail", produces = "application/json")
+    @PostMapping(path = "setEmail", produces = "application/json")
     public void setEmail(@RequestParam long id, @RequestParam String newEmail) {
         userService.setEmail(id, newEmail);
     }
 
-    @PostMapping(path = "/setCompany", produces = "application/json")
+    @PostMapping(path = "setCompany", produces = "application/json")
     public void setCompany(@RequestParam long id, @RequestParam int newCompany) {
         userService.setCompany(id, newCompany);
     }
 
-    @PostMapping(path = "/setStatus", produces = "application/json")
+    @PostMapping(path = "setStatus", produces = "application/json")
     public void setStatus(@RequestParam long id, @RequestParam String newStatus) {
         userService.setStatus(id, newStatus);
     }
