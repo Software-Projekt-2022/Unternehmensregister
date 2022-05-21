52d04edadd538e81d85fd6f6b16a90bbc29a4cae
Clemens <dubcmsky@protonmail.com>
2022-05-21 14:23:59 +0200

    update Cross Origin

--- RestApi/src/main/java/org/cybercity/unternehmensregister/restapi/Controller/CompanyController.java
+++ RestApi/src/main/java/org/cybercity/unternehmensregister/restapi/Controller/CompanyController.java
@@ -13,7 +13,7 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/api/company")
-@CrossOrigin(origins = "localhost")
+@CrossOrigin(origins = "*")
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class CompanyController {
 
--- RestApi/src/main/java/org/cybercity/unternehmensregister/restapi/Controller/UserController.java
+++ RestApi/src/main/java/org/cybercity/unternehmensregister/restapi/Controller/UserController.java
@@ -13,7 +13,7 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/api/user")
-@CrossOrigin(origins = "localhost")
+@CrossOrigin(origins = "*")
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class UserController {
 
