001279c5953f9d21c4ce667fde4b6b3d90522236
Clemens <dubcmsky@protonmail.com>
2022-05-21 12:47:48 +0200

    update CompanyController consume and produce information

--- RestApi/src/main/java/org/cybercity/unternehmensregister/restapi/Controller/CompanyController.java
+++ RestApi/src/main/java/org/cybercity/unternehmensregister/restapi/Controller/CompanyController.java
@@ -19,48 +19,53 @@ public class CompanyController {
 
     private final CompanyService companyService;
 
-    @ResponseBody
-    @PostMapping(path = "newCompany", consumes = "application/json", produces = "application/json")
-    public Company newCompany(@RequestBody Company company) {
-        return companyService.newCompany(company);
-    }
-
     @GetMapping(path = "getAll", produces = "application/json")
+    @ResponseBody
     public List<Company> getAll() {
         return companyService.getAll();
     }
 
     @GetMapping(path = "getByID/{id}", produces = "application/json")
+    @ResponseBody
     public Company getByID(@PathVariable int id) {
         return companyService.getByID(id);
     }
 
     @GetMapping(path = "getByName/{input}", produces = "application/json")
+    @ResponseBody
     public Company getByName(@PathVariable String input) {
         return companyService.getByName(input);
     }
 
     @GetMapping(path = "getByAbbreviation/{input}", produces = "application/json")
+    @ResponseBody
     public Company getByAbbreviation(@PathVariable String input) {
         return companyService.getByAbbreviation(input);
     }
 
     @GetMapping(path = "getByCeoId/{id}", produces = "application/json")
+    @ResponseBody
     public List<Company> getByCeoId(@PathVariable int id) {
         return companyService.getByCeoId(id);
     }
 
-    @PostMapping(path = "setName/{id}", produces = "application/json")
+    @PostMapping(path = "newCompany", consumes = "application/json", produces = "application/json")
+    @ResponseBody
+    public Company newCompany(@RequestBody Company company) {
+        return companyService.newCompany(company);
+    }
+
+    @PostMapping(path = "setName/{id}")
     public void setName(@PathVariable long id, @RequestParam String newName) {
         companyService.setName(id, newName);
     }
 
-    @PostMapping(path = "setAbbreviation/{id}", produces = "application/json")
+    @PostMapping(path = "setAbbreviation/{id}")
     public void setAbbreviation(@PathVariable long id, @RequestParam String newAbbreviation) {
         companyService.setAbbreviation(id, newAbbreviation);
     }
 
-    @PostMapping(path = "setCeoId/{id}", produces = "application/json")
+    @PostMapping(path = "setCeoId/{id}")
     public void setCeoId(@PathVariable long id, @RequestParam int newCeoId) {
         companyService.setCeoId(id, newCeoId);
     }
