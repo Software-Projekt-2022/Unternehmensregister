72681d481d02dc0e39f72d9177944b796b65a83b
Clemens <dubcmsky@protonmail.com>
2022-05-22 21:15:32 +0200

    removed dumb old register function

--- frontend/pages/register.js
+++ frontend/pages/register.js
@@ -19,7 +19,7 @@ const noSuccess = () => {
 const doRegister = async () => {
         const response = await $.ajax({
             url: 'http://localhost:8085/api/user/newUser',
-            method: 'POST',
+            method: 'post',
             dataType: 'json',
             success: function (data) {
                 console.log("You've successfully registered yourself on the website")
@@ -30,28 +30,6 @@ const doRegister = async () => {
             }
         });
 };
-
-const db_register = async () => {
-
-    console.log("Trying to register the account...")
-
-    var user = document.getElementById('inp_email').innerHTML
-    var pw = document.getElementById('inp_pw').innerHTML
-
-    console.log(user, pw)
-
-    $.ajax({
-        type: "POST",
-        url: "127.0.0.1:8080/api/user/newUser",
-        dataType: 'json', 
-        contentType: 'application/json', 
-        data: '{"user":user, "password":pw  }',
-        success: function(data) {
-            console.log(data);
-        },
-        dataType: JSON
-    });
-}
 
 const Register = props => {
     const router = useRouter();
@@ -104,8 +82,6 @@ const Register = props => {
                 </div>
             </nav>
 
-            <button type="button" onClick={hi}>TEST</button>
-
             <section className="position-relative py-4 py-xl-5">
                 <div className="container">
                     <div className="row mb-5">
@@ -125,13 +101,14 @@ const Register = props => {
                                                 d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"></path>
                                         </svg>
                                     </div>
+                                    <div id="api_check"></div>
                                     <form className="text-center" method="post">
                                         <div className="mb-3"><input className="form-control" type="email" name="email"
                                                                      placeholder="Email" id="inp_email"/></div>
                                         <div className="mb-3"><input className="form-control" type="password"
                                                                      name="password" placeholder="Password" id="inp_pw"/></div>
                                         <div className="mb-3">
-                                            <button className="btn btn-primary d-block w-100" type="button" onClick={db_register}>Register
+                                            <button className="btn btn-primary d-block w-100" type="button" onClick={doRegister}>Register
                                             </button>
                                         </div>
                                         <p className="text-muted">Forgot your password?</p>
