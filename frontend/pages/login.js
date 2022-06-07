12cfe7dc8779244d7112f9cf45821e632d052875
Clemens <dubcmsky@protonmail.com>
2022-06-07 02:06:16 +0200

    update login

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import $ from "jquery";
-import LoginForm from "../components/LoginForm.js";
 
 const db_login = () => {
   $.ajax({
