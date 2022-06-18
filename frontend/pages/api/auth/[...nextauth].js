6b4333014df71efdb2fb42dbd531285bb8374d92
Clemens <dubcmsky@protonmail.com>
2022-06-18 19:25:45 +0200

    added credentials provider

--- frontend/pages/api/auth/[...nextauth].js
+++ frontend/pages/api/auth/[...nextauth].js
@@ -1,13 +1,30 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export default NextAuth({
 
   providers: [
+    CredentialsProvider({
+      name: "CyberCity",
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "Username" },
+        password: {  label: "Password", type: "password" }
+      },
+      async authorize(credentials, req) {
+
+        const user = { id: 1, name: "Dubsky", email: "dubsky@example.com", status: "CEO" }
+  
+        if (user) {
+          return user
+        } else {
+          return null
+        }
+      }
+    }),
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
-    }),
-
+    })
   ],
 })
