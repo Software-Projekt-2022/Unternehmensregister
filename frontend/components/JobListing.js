fb485e1f0377125619f6d87dc96f1548d0e7379a
Clemens <dubcmsky@protonmail.com>
2022-06-21 01:27:32 +0200

    fixed row spacing in job listings

--- frontend/components/JobListing.js
+++ frontend/components/JobListing.js
@@ -6,7 +6,7 @@ export default function JobListing(props) {
       <div className="card mb-3">
         <div className="card-body">
           <div className="d-flex flex-column flex-lg-row">
-            <div className="row flex-fill">
+            <div className="row flex-fill justify-content-center">
               <div className="col-sm-3">
                 <h4 className="h5">{props.name}</h4>
                 <span className="badge bg-success fs-6">
@@ -14,7 +14,7 @@ export default function JobListing(props) {
                 </span>
               </div>
               <div className="col-sm-1 py-2 fs-3">
-              <button className="btn btn-primary fs-6 me-2 py-1 px-3">
+                <button className="btn btn-primary fs-6 me-2 py-1 px-3">
                   Employer
                 </button>{" "}
               </div>
