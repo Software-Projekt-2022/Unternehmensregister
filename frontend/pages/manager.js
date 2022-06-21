03740b21586c376c01f29273d29e2eea28d94c06
Clemens <dubcmsky@protonmail.com>
2022-06-21 03:31:08 +0200

    added manager which was forgotten :)

---
+++ frontend/pages/manager.js
@@ -0,0 +1,26 @@
+import Layout from "../components/Layout";
+import _error from "./_error";
+import { Component } from "react";
+import Router, { withRouter } from "next/router";
+import Protected from "../components/Protected";
+import ApplicationForm from "../components/ApplicationForm";
+
+class Manager extends Component {
+  static getInitialProps = async ({ query }) => {
+    return { data: query };
+  };
+
+  render() {
+    const { data } = this.props;
+
+    return (
+      <Layout title="Bewerbung">
+        <Protected>
+          <ApplicationForm job_id={data.job_id} emp_id={data.emp_id} job_name={data.name} />
+        </Protected>
+      </Layout>
+    );
+  }
+}
+
+export default withRouter(Manager);
