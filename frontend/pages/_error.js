c1ebc360367b91c2a27fd7127d39138a1ee4750c
Clemens <dubcmsky@protonmail.com>
2022-06-27 00:47:00 +0200

    updated error page

--- frontend/pages/_error.js
+++ frontend/pages/_error.js
@@ -1,12 +1,13 @@
-import Layout from '../components/Layout';
+import Layout from "../components/Layout";
 
 const _error = () => {
-    return (
-        <Layout title="Error">
-        <h1>Error</h1>
-        <p>STOFF</p>
+  return (
+    <Layout title="Error">
+        <div className="d-flex align-items-center justify-content-center vh-100 bg-danger">
+          <h1 className="display-1 fw-bold text-white">404</h1>
+        </div>
     </Layout>
-    )
-}
+  );
+};
 
 export default _error;
